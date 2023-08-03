8f3df3156bff896b3160848c2b806c4b35378f08
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-04 03:03:14 +0500

    Build-Configuring multiple settings files

--- manage.py
+++ manage.py
@@ -2,11 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblogger.settings')
+
+    if base.DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +23,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
