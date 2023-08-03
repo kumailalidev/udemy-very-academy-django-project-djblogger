6141f589a08815da7b5d0bf3b5d5053e0ba794a0
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-04 03:37:17 +0500

    Fix-moved settings folder inside djblogger folder

--- manage.py
+++ manage.py
@@ -2,16 +2,16 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from settings import base
+from djblogger.settings import base
 
 
 def main():
     """Run administrative tasks."""
 
     if base.DEBUG:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.local")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
