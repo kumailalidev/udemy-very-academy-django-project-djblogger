1bdecefdd0fb48690559dcdc7cf66ba1fa05c84f
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-04 19:14:44 +0500

    Build-Building the blog models

--- djblogger/blog/models.py
+++ djblogger/blog/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Blog post model.
+    """
+
+    options = (
+        {"draft", "Draft"},
+        {"published", "Published"},
+    )
+
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post_author"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    status = models.CharField(max_length=10, choices=options, default="draft")
+
+    class Meta:
+        ordering = "-created_at"
+
+    def __str__(self):
+        return self.name
