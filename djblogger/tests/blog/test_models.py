3f4782e64c1e4b040501b2a04f6631719a949678
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-05 20:03:20 +0500

    Testing-Homepage

--- djblogger/tests/blog/test_models.py
+++ djblogger/tests/blog/test_models.py
@@ -5,5 +5,11 @@ pytestmark = pytest.mark.django_db
 
 class TestPostModel:
     def test_str_return(self, post_factory):
-        post = post_factory(title="test-post")
+        post = post_factory(
+            title="test-post"
+        )  # overriding title defined in factories.py
         assert post.__str__() == "test-post"
+
+    def test_add_tag(self, post_factory):
+        x = post_factory(title="test-post", tags=["test-tag"])
+        assert x.tags.count() == 1
--- djblogger/tests/factories.py
+++ djblogger/tests/factories.py
@@ -31,3 +31,11 @@ class PostFactory(factory.django.DjangoModelFactory):
     author = factory.SubFactory(UserFactory)
     content = "x"
     status = "published"
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            self.tags.add(*extracted)
